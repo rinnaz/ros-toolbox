c58fcac6bc424229d38d910b91b891c20f36af89
Rinat Nazarov <rinat.nazarovv@gmail.com>
2021-10-23 21:52:02 +0300

    Update license

--- rnrt_joint_trajectory_controller/src/joint_trajectory_controller.cpp
+++ rnrt_joint_trajectory_controller/src/joint_trajectory_controller.cpp
@@ -1,4 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2021, Rinat Nazarov
 // Copyright (C) 2013, PAL Robotics S.L.
 // Copyright (c) 2008, Willow Garage, Inc.
 //
