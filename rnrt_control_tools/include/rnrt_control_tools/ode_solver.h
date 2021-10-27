184599f7e71ce75ad4c94ab21ddec191341eded9
Rinat Nazarov <rinat.nazarovv@gmail.com>
2021-10-27 17:33:24 +0300

    Add transfer function class placeholder and clean up code

--- rnrt_control_tools/include/rnrt_control_tools/ode_solver.h
+++ rnrt_control_tools/include/rnrt_control_tools/ode_solver.h
@@ -23,6 +23,25 @@ enum class SolverType
     RUNGEKUTTA
 };
 
+class TransferFcn
+{
+public:
+    TransferFcn();
+    TransferFcn(const vector<double>& num, const vector<double>& den)
+        : m_numerator{num}, m_denominator{den}
+    {};
+
+    ~TransferFcn();
+    void setNumerator(const vector<double>& num){m_numerator = num;};
+    void setDenominator(const vector<double>& den){m_denominator = den;};
+    vector<double> getNumerator(){return m_numerator;};
+    vector<double> getDenominator(){return m_denominator;};
+
+private:
+    vector<double> m_numerator;
+    vector<double> m_denominator;
+};
+
 class OdeSolver
 {
 public:
@@ -30,18 +49,19 @@ public:
     friend ostream &operator<<(ostream &out, const OdeSolver &a);
 
     OdeSolver();
-    OdeSolver(const vector<double> list);
+    OdeSolver(const vector<double> inputEquation);
     void initialize(const vector<double> inputEquation);
     void solve(double computationTime, double stepValue, SolverType = SolverType::EULER, ostream &out = cout);
 
 private:
     vector<double> m_equation;
     vector<double> m_currentCondition;
-    uint32_t m_order;
+    uint64_t m_order;
     double m_stepSize;
+    vector<double> m_k1, m_k2, m_k3, m_k4;
 
-    vector<double> normalize(const vector<double> inputEquation);
-    vector<double> computeDerivatives(const vector<double> condition);
+    vector<double> normalize(const vector<double> inputEquation) const;
+    vector<double> computeDerivatives(const vector<double> condition) const;
     void eulerCompute();
     void rungekuttaCompute();
     void (OdeSolver::*compute)();
