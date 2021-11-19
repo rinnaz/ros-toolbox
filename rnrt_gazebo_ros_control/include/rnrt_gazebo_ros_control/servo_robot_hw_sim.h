e96401f07b147990aafd59659ce44c7436d495a1
Rinat Nazarov <rinat.nazarovv@gmail.com>
2021-11-19 16:12:14 +0300

    Add servo responce computation before applying effort to joints
    [WIP] Need to be tested

--- rnrt_gazebo_ros_control/include/rnrt_gazebo_ros_control/servo_robot_hw_sim.h
+++ rnrt_gazebo_ros_control/include/rnrt_gazebo_ros_control/servo_robot_hw_sim.h
@@ -65,81 +65,87 @@
 // URDF
 #include <urdf/model.h>
 
-
+#include "rnrt_control_tools/current_servo.h"
 
 namespace gazebo_ros_control
 {
 
-class ServoRobotHWSim : public gazebo_ros_control::RobotHWSim
-{
-public:
+  class ServoRobotHWSim : public gazebo_ros_control::RobotHWSim
+  {
+  public:
+    virtual bool initSim(
+        const std::string &robot_namespace,
+        ros::NodeHandle model_nh,
+        gazebo::physics::ModelPtr parent_model,
+        const urdf::Model *const urdf_model,
+        std::vector<transmission_interface::TransmissionInfo> transmissions);
 
-  virtual bool initSim(
-    const std::string& robot_namespace,
-    ros::NodeHandle model_nh,
-    gazebo::physics::ModelPtr parent_model,
-    const urdf::Model *const urdf_model,
-    std::vector<transmission_interface::TransmissionInfo> transmissions);
+    virtual void readSim(ros::Time time, ros::Duration period);
 
-  virtual void readSim(ros::Time time, ros::Duration period);
+    virtual void writeSim(ros::Time time, ros::Duration period);
 
-  virtual void writeSim(ros::Time time, ros::Duration period);
+    virtual void eStopActive(const bool active);
 
-  virtual void eStopActive(const bool active);
+  protected:
+    // Methods used to control a joint.
+    enum ControlMethod
+    {
+      EFFORT,
+      POSITION,
+      POSITION_PID,
+      VELOCITY,
+      VELOCITY_PID
+    };
 
-protected:
-  // Methods used to control a joint.
-  enum ControlMethod {EFFORT, POSITION, POSITION_PID, VELOCITY, VELOCITY_PID};
+    // Register the limits of the joint specified by joint_name and joint_handle. The limits are
+    // retrieved from joint_limit_nh. If urdf_model is not NULL, limits are retrieved from it also.
+    // Return the joint's type, lower position limit, upper position limit, and effort limit.
+    void registerJointLimits(const std::string &joint_name,
+                             const hardware_interface::JointHandle &joint_handle,
+                             const ControlMethod ctrl_method,
+                             const ros::NodeHandle &joint_limit_nh,
+                             const urdf::Model *const urdf_model,
+                             int *const joint_type, double *const lower_limit,
+                             double *const upper_limit, double *const effort_limit);
 
-  // Register the limits of the joint specified by joint_name and joint_handle. The limits are
-  // retrieved from joint_limit_nh. If urdf_model is not NULL, limits are retrieved from it also.
-  // Return the joint's type, lower position limit, upper position limit, and effort limit.
-  void registerJointLimits(const std::string& joint_name,
-                           const hardware_interface::JointHandle& joint_handle,
-                           const ControlMethod ctrl_method,
-                           const ros::NodeHandle& joint_limit_nh,
-                           const urdf::Model *const urdf_model,
-                           int *const joint_type, double *const lower_limit,
-                           double *const upper_limit, double *const effort_limit);
+    unsigned int n_dof_;
 
-  unsigned int n_dof_;
+    hardware_interface::JointStateInterface js_interface_;
+    hardware_interface::EffortJointInterface ej_interface_;
+    hardware_interface::PositionJointInterface pj_interface_;
+    hardware_interface::VelocityJointInterface vj_interface_;
 
-  hardware_interface::JointStateInterface    js_interface_;
-  hardware_interface::EffortJointInterface   ej_interface_;
-  hardware_interface::PositionJointInterface pj_interface_;
-  hardware_interface::VelocityJointInterface vj_interface_;
+    joint_limits_interface::EffortJointSaturationInterface ej_sat_interface_;
+    joint_limits_interface::EffortJointSoftLimitsInterface ej_limits_interface_;
+    joint_limits_interface::PositionJointSaturationInterface pj_sat_interface_;
+    joint_limits_interface::PositionJointSoftLimitsInterface pj_limits_interface_;
+    joint_limits_interface::VelocityJointSaturationInterface vj_sat_interface_;
+    joint_limits_interface::VelocityJointSoftLimitsInterface vj_limits_interface_;
 
-  joint_limits_interface::EffortJointSaturationInterface   ej_sat_interface_;
-  joint_limits_interface::EffortJointSoftLimitsInterface   ej_limits_interface_;
-  joint_limits_interface::PositionJointSaturationInterface pj_sat_interface_;
-  joint_limits_interface::PositionJointSoftLimitsInterface pj_limits_interface_;
-  joint_limits_interface::VelocityJointSaturationInterface vj_sat_interface_;
-  joint_limits_interface::VelocityJointSoftLimitsInterface vj_limits_interface_;
+    std::vector<std::string> joint_names_;
+    std::vector<int> joint_types_;
+    std::vector<double> joint_lower_limits_;
+    std::vector<double> joint_upper_limits_;
+    std::vector<double> joint_effort_limits_;
+    std::vector<ControlMethod> joint_control_methods_;
+    std::vector<control_toolbox::Pid> pid_controllers_;
+    std::vector<CurrentServo> servo_drives_;
+    std::vector<double> joint_position_;
+    std::vector<double> joint_velocity_;
+    std::vector<double> joint_effort_;
+    std::vector<double> joint_effort_command_;
+    std::vector<double> joint_position_command_;
+    std::vector<double> last_joint_position_command_;
+    std::vector<double> joint_velocity_command_;
 
-  std::vector<std::string> joint_names_;
-  std::vector<int> joint_types_;
-  std::vector<double> joint_lower_limits_;
-  std::vector<double> joint_upper_limits_;
-  std::vector<double> joint_effort_limits_;
-  std::vector<ControlMethod> joint_control_methods_;
-  std::vector<control_toolbox::Pid> pid_controllers_;
-  std::vector<double> joint_position_;
-  std::vector<double> joint_velocity_;
-  std::vector<double> joint_effort_;
-  std::vector<double> joint_effort_command_;
-  std::vector<double> joint_position_command_;
-  std::vector<double> last_joint_position_command_;
-  std::vector<double> joint_velocity_command_;
+    std::vector<gazebo::physics::JointPtr> sim_joints_;
 
-  std::vector<gazebo::physics::JointPtr> sim_joints_;
+    std::string physics_type_;
 
-  std::string physics_type_;
+    // e_stop_active_ is true if the emergency stop is active.
+    bool e_stop_active_, last_e_stop_active_;
+  };
 
-  // e_stop_active_ is true if the emergency stop is active.
-  bool e_stop_active_, last_e_stop_active_;
-};
-
-typedef boost::shared_ptr<ServoRobotHWSim> ServoRobotHWSimPtr;
+  typedef boost::shared_ptr<ServoRobotHWSim> ServoRobotHWSimPtr;
 
 }
-
